8a393c8f02c650911e33fee2dfaa1b217524f839
Ajil VC <vcajiltvr@gmail.com>
2024-03-05 13:29:48 +0530

    Buttons in nested grid

---
+++ script.js
@@ -0,0 +1,34 @@
+
+// if(window.matchMedia("(min-width:991px)").matches){
+
+
+// var carouselWidth = $('.CardCarousel')[0].scrollWidth;
+// var cardWidth = $('.cardCarouselItem').width();
+
+// var scrollPosition = 0;
+
+// $('.carousel-control-next').on('click', function(){
+
+//     if(scrollPosition<(cardWidth-(cardWidth*4))){
+
+  
+//         console.log('next');
+//         scrollPosition = scrollPosition+cardWidth;
+//         $('.CardCarousel').animate({scrollLeft:scrollPosition},600);
+
+//     }
+// });
+
+// $('.carousel-control-prev').on('click', function(){
+
+//     if(scrollPosition>0){
+
+
+//     console.log('prev');
+//     scrollPosition = scrollPosition-cardWidth;
+//     $('.CardCarousel').animate({scrollLeft:scrollPosition},600);
+
+// }
+// });
+
+// }
